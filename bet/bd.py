1aeaee16cb9f5f2eff9ab7852cafe0a77de74975
Uriell77 <hermoso77@gmail.com>
2021-03-06 00:26:01 -0400

    falla_de_servidor8

--- bet/bd.py
+++ bet/bd.py
@@ -11,14 +11,12 @@ def crear(datos):
     #ingresa un usuario a la bd parametro datos es tupla en el orden de campos en bd`
     cursor.execute("INSERT INTO usuarios(nombre, correo, password) VALUES ('{0}', '{1}', '{2}')".format(datos[0], datos[1], datos[2]))
     base.commit()
-    cursor.close()
 
 
 def leer(dato):
     #busca registro por nombre o id 
     cursor.execute("SELECT * FROM usuarios WHERE id = ? OR nombre = ?", (dato, dato))
     respuesta = cursor.fetchone()
-    cursor.close()
     return respuesta
 
 
@@ -26,7 +24,6 @@ def leertodo():
     #returna toda la bd
     cursor.execute("SELECT * FROM usuarios")
     respuesta = cursor.fetchall()
-    cursor.close()
     return respuesta
 
 
@@ -34,22 +31,18 @@ def leertodo():
 def editar(id, datos):
     cursor.execute("UPDATE usuarios SET (nombre, correo, password, log, status, saldo) = (?,?,?,?,?,?) WHERE id = ?",(datos[1],datos[2],datos[3],datos[4],datos[5],datos[6], id))
     base.commit()
-    cursor.close()
 
 
 def borrar(id):
     cursor.execute("DELETE FROM usuarios WHERE id = ?",(id))
     base.commit()
-    cursor.close()
 
 
 def existe(datos):
     cursor.execute("SELECT * FROM usuarios WHERE (nombre = ? OR correo = ?) AND password = ?", (datos[0], datos[0], datos[1]))
     if cursor.fetchall():
-        cursor.close()
         return True
     else:
-        cursor.close()
         return False
 
 def log(dato):
